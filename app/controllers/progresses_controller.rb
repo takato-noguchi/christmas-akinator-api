42eba2cada64384a74862c43a6a66a8bc26771fc
takato-noguchi <takato.noguchi23@gmail.com>
2022-12-18 03:31:12 +0900

    add progress settings

---
+++ app/controllers/progresses_controller.rb
@@ -0,0 +1,54 @@
+class ProgressesController < ApplicationController
+  def new 
+    @progress = Progress.new
+    current_game = Game.find(params[:game_id])
+    @question = Question.next_question(current_game)
+  end
+
+  def create
+    current_game = Game.find(params[:game_id])
+    
+    # 回答した内容を保存する 
+    progress = current_game.progresses.new(create_params)
+    progress.assign_sequence
+    progress.save!
+
+    # 絞り込みを実行
+    @extract_presents = ExtractionAlgorithm.new(current_game).compute
+
+    # 絞り込み結果が0件の場合、ギブアップ画面へ遷移
+    if @extract_comics.count == 0
+      redirect_to give_up_game_path(current_game) #リダイレクト先変更
+      return
+    end
+
+    # 絞り込み結果が1件の場合、チャレンジ(正解を当てにいく)へ遷移
+    if @extract_presents.count == 1
+      redirect_to challenge_game_path(current_game) # リダイレクト先変更
+      return
+    end
+
+    if @extract_comics.count >= 2
+      next_question = Question.next_question(current_game)
+      if next_question.blank?
+
+        current_game.status = 'finished'
+        current_game.result = 'incorrect'
+        current_game.save!
+
+        redirect_to give_up_game_path(current_game)
+        return
+      end
+
+       redirect_to new_game_progresses_path(current_game) # jsonを調べる
+      return
+    end
+  end
+  
+  private
+
+  def create_params
+    params.require(:progress).permit(:question_id, :answer)
+  end
+
+end
--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,2 +1,23 @@
 class Progress < ApplicationRecord
+  belongs_to :game
+  belongs_to :question
+
+  def assign_sequence
+    next_sequence = 1
+    if game.present?
+      all_progress = game.progresses
+      if all_progress.count > 0
+        next_sequence = all_progress.maximum(:sequence) + 1
+      end
+    end
+    self.sequence = next_sequence
+  end
+
+  def positive_answer?
+    answer == 'positive'
+  end
+
+  def negative_answer?
+    answer == 'negative'
+  end
 end
