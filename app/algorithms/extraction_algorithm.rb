3e43eda21335142c75e4ad08e0a4495ad9f8004c
takato-noguchi <takato.noguchi23@gmail.com>
2022-12-18 08:03:16 +0900

    add algorithm pattern

--- app/algorithms/extraction_algorithm.rb
+++ app/algorithms/extraction_algorithm.rb
@@ -15,8 +15,10 @@ class ExtractionAlgorithm
         question = progress.question
   
         case question.algorithm
-          when ''
-            ?(progress)
+          when 'genre_match'
+            genre_match(progress)
+          when 'category_match'
+            category_match(progress)
           else
             raise Exception('Invalid algorithm. --> ' + question.algorithm.to_s)
         end
@@ -32,13 +34,37 @@ class ExtractionAlgorithm
     def serialization_end?(progress)
   
       if progress.positive_answer?
-        @query = @query.where.not("presents. is null")
+        @query = @query.where.not("presents.category is null")
       end
   
       if progress.negative_answer?
-        @query = @query.where("presents. is null")
+        @query = @query.where("presents.category is null")
       end
   
+    end
+
+    def genre_match(progress)
+
+        if progress.positive_answer?
+          @query = @query.where("present.genre like ?", "%#{progress.question.eval_value}%")
+        end
+    
+        if progress.negative_answer?
+          @query = @query.where.not("presents.genre like ?", "%#{progress.question.eval_value}%")
+        end
+    
+    end
+
+    def publisher_match(progress)
+
+        if progress.positive_answer?
+           @query = @query.where("presents.category like ?", "%#{progress.question.eval_value}%")
+        end
+   
+        if progress.negative_answer?
+           @query = @query.where.not("comics.category like ?", "%#{progress.question.eval_value}%")
+        end
+   
     end
   end
   
