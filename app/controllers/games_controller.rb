17a935562996bcb313889241b5cf8a4902bd89be
takato-noguchi <takato.noguchi23@gmail.com>
2022-12-18 03:29:58 +0900

    add game settings

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+class GamesController < ApplicationController
+  
+  def create
+    game = Game.create!(status: 'in_progress')
+    redirect_to new_game_progresses_path(game)
+  end
+
+  def challenge
+    current_game = Game.find(params[:id])
+    extract_presents =  ExtractionAlgorithm.new(current_game).compute
+    @present = extract_presents.first
+  end
+
+  def give_up
+  end
+
+  def update
+    current_game = Game.find(params[:id])
+
+    if params[:correct].present?
+      current_game.status = 'finished'
+      current_game.result = :correct
+      current_game.save!
+
+      redirect_to correct_game_path(current_game) #リダイレクト先変更
+    else
+      current_game.status = 'finished'
+      current_game.result = :incorrect
+      current_game.save!
+
+      redirect_to give_up_game_path(current_game) #リダイレクト先変更
+    end
+  end
+
+  def correct
+  end
+end
