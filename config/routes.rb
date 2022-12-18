6a271f901dbcd0a93eb6f949f429bde7ef68881c
takato-noguchi <takato.noguchi23@gmail.com>
2022-12-18 09:52:10 +0900

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'games#new'
+  root to: 'games#new'
     resources :games, only: %i[new create update] do
       member do
         get 'give_up', to: 'games#giveup'
